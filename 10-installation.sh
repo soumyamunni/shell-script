f55e9af445871277e873ce365011ef03c8f207b1
soumya <soumyamunni510@gmail.com>
2024-05-17 13:14:43 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -14,10 +14,20 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "installation of mysql...FAILURE"
+    echo "Installation of mysql...FAILURE"
     exit 1
+else
+    echo"Installation of mysql...success"
 fi
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo"Installation of git...success"
+fi
 
 echo "is script proceeding?"

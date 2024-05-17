97883f568acd83b5e5077c1ffdf34a912dff9ac3
soumya <soumyamunni510@gmail.com>
2024-05-17 09:21:12 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #Here USERNAME is variable 
+
+echo "please enter password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME,password is: $PASSWORD
---
+++ 08-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo"All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "which user is running this script: $USER"
+echo "Hostname : $HOSTNAME"
+echo "process ID of the current shell script: $$"
+sleep 60 &
+echo "Process ID of last background command: $!"

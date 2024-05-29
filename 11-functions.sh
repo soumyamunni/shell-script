2f43bf6a67aa42da1128f4347e3afa744629c16f
soumya <soumyamunni510@gmail.com>
2024-05-29 13:29:29 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,10 +5,10 @@ USERID=$(id -u)
 VALIDATE(){
     if[ $1 -ne 0]
     then
-         echo "$2...FAILURE"
-         exit 1
+        echo "$2...FAILURE"
+        exit 1
     else
-         echo "$2...SUCCESS"
+        echo "$2...SUCCESS"
     fi     
 
 }
